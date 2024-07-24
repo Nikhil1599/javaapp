7e8ad7a242aefcee73360736573f6870392f9911
Nikhil1599 <nikhilrsh9320281778@gmail.com>
2024-07-24 15:32:51 +0530

    m1

---
+++ src/main/java/javaapp/main.java
@@ -0,0 +1,12 @@
+package javaapp;
+
+import java.util.Iterator;
+
+public class main {
+	public static void main(String[] args) {
+		String name = "Hello";
+		for(int i = 0; i < 100; i++) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ target/javaapp/about.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>This is about page</h1>
+	<a href="index.jsp">go back to index page</a>
+</body>
+</html>
